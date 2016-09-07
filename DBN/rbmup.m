78b953930ceaf953d2f0f1a24d701e8867a564a5
dohuan <dohuan@msu.edu>
2016-09-07 11:14:39 -0400

    090716

--- DBN/rbmup.m
+++ DBN/rbmup.m
@@ -1,3 +1,8 @@
 function x = rbmup(rbm, x)
     x = sigm(repmat(rbm.c', size(x, 1), 1) + x * rbm.W');
+%     tmp = (repmat(rbm.c', size(x, 1), 1) + x * rbm.W');
+%     for i=1:size(tmp,1)
+%         tmp(i,:) = tmp(i,:)-mean(tmp(i,:));
+%     end
+%     x = sigm(tmp);
 end
--- NN/nntrain.m
+++ NN/nntrain.m
@@ -61,16 +61,16 @@ for i = 1 : numepochs
 
     if opts.validation == 1
         loss = nneval(nn, loss, train_x, train_y, val_x, val_y);
-        str_perf = sprintf('; Full-batch train mse = %f, val mse = %f', loss.train.e(end), loss.val.e(end));
+        str_perf = sprintf(';\n Full-batch train mse = %f, val mse = %f', loss.train.e(end), loss.val.e(end));
     else
         loss = nneval(nn, loss, train_x, train_y);
-        str_perf = sprintf('; Full-batch train err = %f', loss.train.e(end));
+        str_perf = sprintf(';\n Full-batch train err = %f', loss.train.e(end));
     end
     if ishandle(fhandle)
         nnupdatefigures(nn, fhandle, loss, opts, i);
     end
         
-    disp(['epoch ' num2str(i) '/' num2str(opts.numepochs) '. Took ' num2str(t) ' seconds' '. Mini-batch mean squared error on training set is ' num2str(mean(L((n-numbatches):(n-1)))) str_perf]);
+    disp(['epoch ' num2str(i) '/' num2str(opts.numepochs) '. Took ' num2str(t) ' seconds.' '\n Mini-batch mean squared error on training set is ' num2str(mean(L((n-numbatches):(n-1)))) str_perf]);
     nn.learningRate = nn.learningRate * nn.scaling_learningRate;
 end
 end
